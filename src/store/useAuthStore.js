7b0853eeb6d6c19cd1843807220105bb47eeafeb
Raghib <raghibanis72@gmail.com>
2025-07-23 17:45:32 +0530

    fixed rerendering

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -17,7 +17,8 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
-
+      console.log("checkAuth response:", res.data);
+      
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
