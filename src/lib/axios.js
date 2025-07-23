c9877ca540d83c79db1a3b46977956e3b0fc5be7
Raghib <raghibanis72@gmail.com>
2025-07-23 18:19:20 +0530

    fixed api calling and backend connection

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatty-backend-0gsi.onrender.com/api" : "/api",
+  baseURL: "https://chatty-backend-0gsi.onrender.com/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chatty-backend-0gsi.onrender.com" : "/";
+const BASE_URL = "https://chatty-backend-0gsi.onrender.com"
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -17,8 +17,7 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
-      console.log("checkAuth response:", res.data);
-      
+
       set({ authUser: res.data });
       get().connectSocket();
     } catch (error) {
